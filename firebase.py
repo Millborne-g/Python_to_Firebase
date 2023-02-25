372f97e7dbcfb16863b3044e31fba96cca0e404b
Millborne-g <galamiton.millborne@gmail.com>
2023-02-26 03:49:03 +0800

    first commit

---
+++ firebase.py
@@ -0,0 +1,29 @@
+import pyrebase
+from datetime import date
+import time
+from datetime import datetime
+
+firebaseConfig = {
+  "apiKey": "AIzaSyB_4cNoh3klH4mKPSd7dhJzr5QUGoLihy8",
+  "authDomain": "scanmemaster-9da58.firebaseapp.com",
+  "projectId": "scanmemaster-9da58",
+  "databaseURL" : "https://scanmemaster-9da58-default-rtdb.firebaseio.com/",
+  "storageBucket": "scanmemaster-9da58.appspot.com",
+  "messagingSenderId": "270970295536",
+  "appId": "1:270970295536:web:02ecd24ee665578e6d9e35",
+  "measurementId": "G-27WEKS22GB"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+now = datetime.now()
+
+dateToday = str(date.today())
+timeToday = now.strftime("%H:%M:%S")
+
+data = {"Location": "Lapasan Zone 2", "Date": dateToday, "Time": timeToday, "Notification": "on"}
+#-------------------------------------------------------------------------------
+# Create Data
+
+db.child("Scanned").child("000-111-222").child(dateToday).set(data)
