5faa0f99828f88b38430cab3c843c291e95cbb36
Millborne-g <galamiton.millborne@gmail.com>
2023-03-12 02:12:58 +0800

    commit

--- firebase.py
+++ firebase.py
@@ -19,26 +19,41 @@ db = firebase.database()
 
 def submitPlateNumber(PN):
     try:
-      exist = db.child("Vehicle_with_criminal_offense").child(PN).child("plateNumber").get()
-      print(exist.val())
-      if exist.val() != None:
-          isApprehended = db.child("Vehicle_with_criminal_offense").child(PN).child("apprehended").get()
-          print("isApprehended "+isApprehended.val())
-          if isApprehended.val() != 'yes':
-            # Create Data
-            now = datetime.now()
-            dateToday = str(date.today())
-            timeToday = now.strftime("%H:%M:%S")
-            crimeScanned = db.child("Vehicle_with_criminal_offense").child(PN).child("criminalOffense").get()
-            data = {"PlateNumber":PN, "Location": "Lapasan Zone 4", "Date": dateToday, "Time": timeToday, "Notification": "on", "Apprehended": "no", "CriminalOffense": crimeScanned.val()}
-            db.child("Scanned").child((dateToday+" "+timeToday)).set(data)
-            crime = db.child("Vehicle_with_criminal_offense").child(PN).child("criminalOffense").get()
-            dataPlateNumber = {"PlateNumber":PN, "Apprehended": "no","CriminalOffense": crime.val()}
-            db.child("ScannedPlateNmber").child(PN).set(dataPlateNumber)
-      else:
-          print("Plate Number dont't exist")
-    except:
-        print("Plate Number dont't exist")
+        now = datetime.now()
+        dateToday = str(date.today())
+        timeToday = now.strftime("%H:%M:%S")
+        isApprehended = db.child("Vehicle_with_criminal_offense").child(PN).child("apprehended").get()
+        crimeScanned = db.child("Vehicle_with_criminal_offense").child(PN).child("criminalOffense").get()
+
+        # Scanned
+        data = {"PlateNumber":PN, "Location": "Lapasan Zone 4", "Date": dateToday, "Time": timeToday, "Notification": "on", "Apprehended": isApprehended.val(), "CriminalOffense": crimeScanned.val()}
+        db.child("Scanned").child((dateToday+" "+timeToday)).set(data)
+
+        # ScannedPlateNmber
+        dataPlateNumber = {"PlateNumber":PN, "Apprehended": isApprehended.val(),"CriminalOffense": crimeScanned.val()}
+        db.child("ScannedPlateNmber").child(PN).set(dataPlateNumber)
+      # exist = db.child("Vehicle_with_criminal_offense").child(PN).child("plateNumber").get()
+      # print(exist.val())
+      # if exist.val() != None:
+      #     isApprehended = db.child("Vehicle_with_criminal_offense").child(PN).child("apprehended").get()
+      #     print("isApprehended "+isApprehended.val())
+      #     if isApprehended.val() != 'yes':
+      #       # Create Data
+      #       now = datetime.now()
+      #       dateToday = str(date.today())
+      #       timeToday = now.strftime("%H:%M:%S")
+      #       crimeScanned = db.child("Vehicle_with_criminal_offense").child(PN).child("criminalOffense").get()
+      #       data = {"PlateNumber":PN, "Location": "Lapasan Zone 4", "Date": dateToday, "Time": timeToday, "Notification": "on", "Apprehended": "no", "CriminalOffense": crimeScanned.val()}
+      #       db.child("Scanned").child((dateToday+" "+timeToday)).set(data)
+      #       crime = db.child("Vehicle_with_criminal_offense").child(PN).child("criminalOffense").get()
+      #       dataPlateNumber = {"PlateNumber":PN, "Apprehended": "no","CriminalOffense": crime.val()}
+      #       db.child("ScannedPlateNmber").child(PN).set(dataPlateNumber)
+      # else:
+      #     print("Plate Number dont't exist")
+        print('plate number submitted to db')
+    except Exception as e:
+        
+        print("Plate Number dont't exist "+ str(e))
 
 #isulod ang ang plate number sa plateNumber na variable gaw
 plateNumber = "321"
